4ee51741368eabdfda441d8b77c21a1ff222e259
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-15 23:59:59 +0100

    Build keystore and gatekeeper HALs [2/2]

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -65,6 +65,8 @@ PRODUCT_PACKAGES += \\
     com.quicinc.cne.constants@2.0 \\
     com.quicinc.cne.server@1.0 \\
     com.quicinc.cne.server@2.0 \\
+    gatekeeper.msm8996 \\
+    keystore.msm8996 \\
     libgpustats \\
     libtime_genoff \\
     datastatusnotification \\
@@ -303,6 +305,32 @@ LOCAL_MULTILIB := both
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := gatekeeper.msm8996
+LOCAL_MODULE_OWNER := zuk
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/hw/gatekeeper.msm8996.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/hw/gatekeeper.msm8996.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := keystore.msm8996
+LOCAL_MODULE_OWNER := zuk
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/hw/keystore.msm8996.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/hw/keystore.msm8996.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
