afda5e37c7868ca18f5667dd557553fc8a53a93b
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-15 21:18:23 +0100

    Update GPS stack from UM6.5 [2/2]

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -71,7 +71,6 @@ PRODUCT_PACKAGES += \\
     QtiTelephonyService \\
     shutdownlistener \\
     TimeService \\
-    com.qualcomm.location \\
     qcrilmsgtunnel \\
     qcrilhook \\
     libqti_performance \\
@@ -176,18 +175,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
