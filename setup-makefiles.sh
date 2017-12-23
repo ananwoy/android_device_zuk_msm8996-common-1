48a3d3d78bdaa2c5f2c7f0aa1ec7c048a2520a16
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-23 16:44:22 +0100

    Update GPS stack from UM6.5 [2/2]

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -67,7 +67,6 @@ PRODUCT_PACKAGES += \\
     shutdownlistener \\
     TimeService \\
     CNEService \\
-    com.qualcomm.location \\
     qcrilmsgtunnel \\
     qcrilhook \\
     ims \\
@@ -110,7 +109,7 @@ LOCAL_PATH := \$(call my-dir)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/lib64/libloc_api_v02.so
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_api_v02.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -177,18 +176,6 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := CNEService
 LOCAL_MODULE_OWNER := zuk
 LOCAL_SRC_FILES := proprietary/priv-app/CNEService/CNEService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
