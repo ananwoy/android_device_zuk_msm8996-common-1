0ff545be8dc2a4b4f226abd9a370b7608a2b5191
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-06 22:50:52 +0100

    Update permgr from LA.UM.6.5.r1-02500-8x96.0 [2/2]

--- config.fs
+++ config.fs
@@ -25,6 +25,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
